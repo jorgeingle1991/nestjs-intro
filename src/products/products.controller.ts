dd2efd7b5a55473b2c8e8728ee1bae083d2f8bdd
Jorge Ingle <JorgeI@boxerproperty.com>
2022-11-24 23:19:59 -0600

    Showing to Kishan

---
+++ src/products/products.controller.ts
@@ -0,0 +1,50 @@
+import { Controller, Post, Body, Get, Param, Patch, Delete } from "@nestjs/common";
+import { ProductsService } from "./products.service";
+
+@Controller('products')
+export class ProductsController {
+    constructor(private readonly productsService: ProductsService) {
+
+    }
+
+    @Post()
+    addProduct(
+        @Body('title') prodTitle: string,
+        @Body('description') prodDesc: string,
+        @Body('price') prodPrice: number
+    ): any {
+        const generatedId = this.productsService.insertProduct(
+            prodTitle,
+            prodDesc,
+            prodPrice
+        );
+        return { id: generatedId }
+    }
+
+    @Get()
+    getAllProducts() {
+        return this.productsService.getProducts();
+    }
+
+    @Get(':id')
+    getProduct(@Param('id') productId: string) {
+        return this.productsService.getSingleProduct(productId);
+    }
+
+    @Patch(':id')
+    updateProduct(
+        @Param('id') prodId: string,
+        @Body('title') prodTitle: string,
+        @Body('description') prodDescription: string,
+        @Body('price') prodPrice: number
+    ) {
+        this.productsService.updateProduct(prodId, prodTitle, prodDescription, prodPrice);
+        return { "msg": "Updated record!" };
+    }
+
+    @Delete(':id')
+    removeProduct(@Param('id') productId: string) {
+        this.productsService.deleteProduct(productId);
+        return { "msg": "Deleted record!" };
+    }
+}
---
+++ src/products/products.model.ts
@@ -0,0 +1,8 @@
+import { TimeInterval } from "rxjs/internal/operators/timeInterval";
+
+export class Product {
+    constructor(public id:string,public title:string,public description:string,public price:number) {
+        
+
+    };
+}
